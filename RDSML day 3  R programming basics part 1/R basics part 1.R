e55ce1d937c06dfb317253e334e006b4cd0f1404
nbictvirtuallab-2 <jannatoulferdous2@gmail.com>
2025-05-08 14:10:37 +0600

    RDSML day 3 codes

---
+++ RDSML day 3  R programming basics part 1/R basics part 1.R
@@ -0,0 +1,10 @@
+#Calculating some health measurements
+
+my_age = 29
+my_weight = 50
+my_height = 62
+x = (my_height*my_age)/my_weight
+print(x)
+student_height = c(25, 26, 27, 28.75)
+mean(student_height)
+
