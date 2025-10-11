0459c8f09d5dfc40198e99f39c3f3f39826c8f6f
nbictvirtuallab-2 <jannatoulferdous2@gmail.com>
2025-10-11 23:31:40 +0600

    day 17
    day 17

---
+++ day 17 error bar in R/day 16.R
@@ -0,0 +1,93 @@
+#Creating a data frame
+dataset = data.frame(dose = c("D0.5", "D1", "D2"), 
+                     len = c(4.2, 10, 29.5))
+
+library(ggplot2)
+
+# Basic barplot
+ggplot(data = dataset, aes(x = dose, y = len)) + 
+  geom_bar(stat = "identity")
+
+# Horizontal barplot
+ggplot(data = dataset, aes(x = dose, y = len)) + 
+  geom_bar(stat = "identity") +
+  coord_flip()
+
+# Changing the width of bars
+ggplot(data = dataset, aes(x = dose, y = len)) + 
+  geom_bar(stat = "identity", width = .5)
+
+# Changing colors
+ggplot(data = dataset, aes(x = dose, y = len)) + 
+  geom_bar(stat = "identity", color = "blue", fill = "white", width = .5)
+
+# Minimal theme with blue fill color
+ggplot(data = dataset, aes(x = dose, y = len)) + 
+  geom_bar(stat = "identity", fill = "steelblue") +
+  theme_minimal()
+
+# Barplot with labels
+# Outeside bars
+ggplot(data = dataset, aes(x = dose, y = len)) + 
+  geom_bar(stat = "identity", fill = "steelblue") +
+  geom_text(aes(label = len), vjust = -0.5, size = 3, color = "#9F9F9F") +
+  theme_minimal()
+
+# Inside bars
+ggplot(data = dataset, aes(x = dose, y = len)) + 
+  geom_bar(stat = "identity", fill = "steelblue") +
+  geom_text(aes(label = len), vjust = 1.6, size = 3, color = "white") +
+  theme_minimal()
+
+myCarData = mtcars
+# Barplot of counts
+# To make a barplot of counts, we will use mtcars dataset
+
+ggplot(data = myCarData, aes(x = factor(cyl))) +
+  geom_bar(stat = "count", width = 0.7, fill = "steelblue") +
+  theme_minimal() 
+
+#Changing barplot line colors by groups
+
+print(dataset)
+ggplot(dataset, aes(x = dose, y = len, color = dose)) +
+  geom_bar(stat = "identity", fill = "white") 
+
+
+#Using custom color palettes
+ggplot(dataset, aes(x = dose, y = len, color = dose)) +
+  geom_bar(stat = "identity", fill = "white") +
+  scale_color_manual(values = c("red","black","orange"))
+
+#Using brewer color paletts
+
+ggplot(dataset, aes(x = dose, y = len, color = dose)) +
+  geom_bar(stat = "identity", fill = "white") +
+  scale_color_brewer(palette = "Dark2")
+
+#using gray scale
+
+ggplot(dataset, aes(x = dose, y = len, color = dose)) +
+  geom_bar(stat = "identity", fill = "white") +
+  scale_color_grey()+
+  theme_classic()
+
+#changing bar plot fill colors by group
+
+p = ggplot(dataset, aes(x = dose, y = len, fill = dose)) +
+  geom_bar(stat = "identity") +
+  theme_minimal() + scale_fill_manual(values = c("#3826bf", "#bf264f", "#5ebf26")) 
+
+p + scale_fill_brewer(palette = "Dark2")
+
+p+ scale_fill_grey()
+
+#Using black outline color
+
+ggplot(dataset, aes(x = dose, y = len, fill = dose)) +
+  geom_bar(stat = "identity", color = "black") +
+  scale_color_manual(values = c("#3826bf", "#bf264f", "#5ebf26"))+
+  theme(legend.position = "bottom")
+
+#you should learn more about GitHub from the internet
+#please go to youtube and search for GitHub free courses
